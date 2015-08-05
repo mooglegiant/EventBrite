4a1e15a6f6fd9a6752e46f1c2c4cd8c9c85913ab
Laura Traveller <laura.traveller@totalenterprisesolutions.co.uk>
2015-08-05 09:22:15 +0100

    Added a Ticket Class

---
+++ TES.Integration.Eventbrite/Classes/Common/Ticket.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TES.Integration.Eventbrite.Classes.Events;
+
+namespace TES.Integration.Eventbrite.Classes.Common
+{
+    class Ticket
+    {
+        public Event MyProperty { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public Currency cost { get; set; }
+        public Currency fee { get; set; }
+        public bool donation { get; set; }
+        public bool free { get; set; }
+        public int maximum_quantity { get; set; }
+        public int minimum_dquantity { get; set; }
+    }
+}
